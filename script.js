ff96c8b653d029893e452bb88e1bbd01de9aeea4
pdpate22 <pdpate22@ncsu.edu>
2025-05-03 02:02:51 -0400

    Added a live checker based on input intobox, need to make it work correctly when attempting to redo a mistake because it only works perfectly when typing the correct word in the first guess or if there is only a mistake with the last character

--- script.js
+++ script.js
@@ -2,31 +2,40 @@ let numLetters;
 let numGivenLetters;
 let generatedWord;
 let gameBox;
+let userAttemptedWord = "";
+
 // numLettersSubmit
 //"daily-attempt-puzzle
+
+// start of game - when number of letters choice is submitted
 document.getElementById("numLettersSubmit").onclick = function () {
   numLetters = document.getElementById("numLetters").value;
   numLetters = Number(numLetters);
   console.log(numLetters);
 
+  // number of given letters
   numGivenLetters = document.getElementById("numGivenLetters").value;
   console.log(numGivenLetters);
 
+  // message for user notification
   document.getElementById("usr-msg").textContent = "Word has been generated";
 
+  // Generate random word
   generatedWord = generateRandomWord(numLetters);
-  document.getElementById("generatedWordToGuess").textContent = generatedWord;
+  document.getElementById("generatedWordToGuess").textContent = generatedWord.toUpperCase();
 
-  gameBox = userGuess();
-  document.getElementById("tempGameBox").textContent = gameBox;
-
-  document.getElementById("gameBoxSubmit").onclick = function () {
-    userAttempt = document.getElementById("game-box").value;
-    console.log(userAttempt);
-    document.getElementById("gameOutcome").textContent = checkWords(userAttempt);
-  };
+  inputBoxes(generatedWord.length);
 };
 
+// when user submits a guess
+document.getElementById("gameBoxSubmit").onclick = function () {
+  //   userAttempt = document.getElementById("innerGameBox").value;
+  //   userAttempt = document.querySelectorAll(".inputBox");
+  //   console.log(userAttempt);
+  //   document.getElementById("gameOutcome").textContent = checkWords(userAttempt);
+};
+
+// generates a random word
 function generateRandomWord(length) {
   const dictionary = [
     "a",
@@ -190,26 +199,73 @@ function generateRandomWord(length) {
   return generatedWord;
 }
 
+// comparision of generated word and user guess
 function checkWords(word) {
   let s = "";
-  for (let i = 0; i < generatedWord.length; i++) {
+  //   for (let i = 0; i < generatedWord.length; i++) {
+  //     if (word.charAt(i) === generatedWord.charAt(i)) {
+  //       s += word.charAt(i) + " ";
+  //     } else {
+  //       s += "_ ";
+  //     }
+  //   }
+  //   if (word === generatedWord) {
+  //     document.getElementById("gameOutcome").textContent = "Correct Guess   " + generatedWord;
+  //   } else {
+  let tempWord = "";
+  for (let i = 0; i < word.length; i++) {
     if (word.charAt(i) === generatedWord.charAt(i)) {
-      s += word.charAt(i) + " ";
+      tempWord += word.charAt(i);
     } else {
-      s += "_ ";
+      tempWord += "X";
     }
   }
-  if (word === generatedWord) {
-    return "Correct Guess   " + generatedWord;
-  } else {
-    return "Incorrect Guess   " + s;
-  }
+  userAttemptedWord = tempWord;
+  console.log("User Attempt: " + userAttemptedWord);
+  document.getElementById("gameOutcome").textContent = "Incorrect Guess   " + s;
+  //}
 }
 
+// displays the amount of correct words from the gues
 function userGuess() {
   let s = "";
   for (let i = 0; i < generatedWord.length - 1; i++) {
     s += "_ ";
   }
   return s + "_";
+}
+
+// input boxes for user
+function inputBoxes(wordLength) {
+  let word = "";
+  const container = document.getElementById("innerGameBox");
+  container.innerHTML = "";
+
+  for (let i = 0; i < wordLength; i++) {
+    const input = document.createElement("input");
+    input.type = "text";
+    input.maxLength = 1;
+    input.classList.add("inputBox");
+
+    input.addEventListener("input", (e) => {
+      if (e.target.value.length === 1 && i < wordLength - 1) {
+        container.children[i + 1].focus();
+        userAttemptedWord += e.target.value;
+        console.log(userAttemptedWord);
+      } else {
+        userAttemptedWord += e.target.value;
+        console.log(userAttemptedWord);
+      }
+      checkWords(userAttemptedWord);
+      //   if (i === wordLength - 1) {
+      //     checkWords(userAttemptedWord);
+      //     console.log(i);
+      //   }
+    });
+    container.appendChild(input);
+  }
+  //word = document.querySelectorAll(".inputBox");
+
+  container.firstChild.focus();
+  //checkWords(word);
 }
